c9f0fc70a3e9256ae9002cf6426dff73ea0ba3b8
AxelMlz <axel.fm.melezan@gmail.com>
2022-02-23 16:08:18 +0100

    setting input for research

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,22 +4,28 @@ import {useState, useEffect} from "react";
 function Home() {
 
 const [cityWeather, setWeather] = useState("")
-const [currentWeather, setCurrentWeather] = useState()
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityWeather},uk&APPID=ac4cb07e6f578cd6cb15563efd460410`)
+const [currentWeather, setCurrentWeather] = useState("")
+const submitValue = () => {
+    const weather = {
+        'Insert a city' : cityWeather,
+    }
+    // ${cityWeather}
+useEffect(()=> {fetch(`http://api.openweathermap.org/data/2.5/weather?q=paris,fr&APPID=ac4cb07e6f578cd6cb15563efd460410`)
 .then((res)=>res.json())
 .then((res)=>{
-    setCurrentWeather(currentWeather.res);
-    console.log(currentWeather);
-})
+    setCurrentWeather(res);
+    console.log(res);
+})})
 
     return (
       <div>
-          <input type ="text" {...cityWeather} placeholder="Insert a city"></input>
-          <button onChange={setWeather}></button>
-          <p></p>
+          <input type ="text" {...cityWeather} placeholder="Insert a city" onChange={e => setWeather(e.target.value)}/>
+
+          <button onClick={submitValue}></button>
+          <p>{currentWeather.main.temp}</p>
+          <p>{currentWeather.weather.icon}</p>
       </div>
     )
 }
-
+}
 export default Home;
